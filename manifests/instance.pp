e431c8f777cb9fc977fdbc6caee56ce4e3166ee6
wstiern <wstiern@pulsepoint.com>
2017-02-17 16:08:34 -0700

    catch up module config with go-carbon 1.9 available featureset

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -11,15 +11,18 @@ define go_carbon::instance(
   $go_maxprocs                     = $go_carbon::params::go_maxprocs,
   $internal_graph_prefix           = $go_carbon::params::internal_graph_prefix,
   $internal_metrics_interval       = $go_carbon::params::internal_metrics_interval,
+  $internal_metrics_endpoint       = $go_carbon::params::internal_metrics_endpoint,
   $max_cpu                         = $go_carbon::params::max_cpu,
   $whisper_data_dir                = $go_carbon::params::whisper_data_dir,
   $whisper_schemas_file            = $go_carbon::params::whisper_schemas_file,
   $whisper_aggregation_file        = $go_carbon::params::whisper_aggregation_file,
   $whisper_workers                 = $go_carbon::params::whisper_workers,
   $whisper_max_updates_per_second  = $go_carbon::params::whisper_max_updates_per_second,
+  $whisper_sparse_create	   = $go_carbon::params::whisper_sparse_create,
   $whisper_enabled                 = $go_carbon::params::whisper_enabled,
   $cache_max_size                  = $go_carbon::params::cache_max_size,
   $cache_input_buffer              = $go_carbon::params::cache_input_buffer,
+  $cache_write_strategy            = $go_carbon::params::cache_write_strategy,
   $udp_listen                      = $go_carbon::params::udp_listen,
   $udp_log_incomplete              = $go_carbon::params::udp_log_incomplete,
   $udp_enabled                     = $go_carbon::params::udp_enabled,
@@ -32,6 +35,17 @@ define go_carbon::instance(
   $carbonlink_enabled              = $go_carbon::params::carbonlink_enabled,
   $carbonlink_read_timeout         = $go_carbon::params::carbonlink_read_timeout,
   $carbonlink_query_timeout        = $go_carbon::params::carbonlink_query_timeout,
+  $carbonserver_listen             = $go_carbon::params::carbonserver_listen,
+  $carbonserver_enabled            = $go_carbon::params::carbonserver_enabled,
+  $carbonserver_buckets            = $go_carbon::params::carbonserver_buckets,
+  $carbonserver_max_globs          = $go_carbon::params::carbonserver_max_globs,
+  $carbonserver_metrics_as_counters = $go_carbon::params::carbonserver_metrics_as_counters,
+  $carbonserver_read_timeout       = $go_carbon::params::carbonserver_read_timeout,
+  $carbonserver_write_timeout      = $go_carbon::params::carbonserver_write_timeout,
+  $carbonserver_scan_frequency     = $go_carbon::params::carbonserver_scan_frequency,
+  $dump_enabled                    = $go_carbon::params::dump_enabled,
+  $dump_path                       = $go_carbon::params::dump_path,
+  $dump_restore_per_second         = $go_carbon::params::dump_restore_per_second,
   $pprof_listen                    = $go_carbon::params::pprof_listen,
   $pprof_enabled                   = $go_carbon::params::pprof_enabled,
 )
@@ -43,13 +57,20 @@ define go_carbon::instance(
     "Invalid log level ${log_level}, Valid values: 'debug', 'info', 'warn', 'warning', 'error'")
   validate_string($internal_graph_prefix)
   validate_string($internal_metrics_interval)
+
+  if $internal_metrics_endpoint {
+    validate_string($internal_metrics_endpoint
+  }
+
   validate_integer($max_cpu)
   validate_absolute_path($whisper_data_dir)
   validate_integer($whisper_workers)
   validate_integer($whisper_max_updates_per_second)
+  validate_bool($whisper_sparse_create)
   validate_bool($whisper_enabled)
   validate_integer($cache_max_size)
   validate_integer($cache_input_buffer)
+  validate_integer($cache_write_strategy)
   validate_integer($go_maxprocs)
   validate_bool($service_enable)
 
@@ -84,6 +105,20 @@ define go_carbon::instance(
   validate_absolute_path($whisper_schemas_file)
   validate_absolute_path($whisper_aggregation_file)
 
+  validate_string($carbonserver_listen)
+  validate_bool($carbonserver_enabled)
+  validate_integer($carbonserver_buckets)
+  validate_integer($carbonserver_max_globs)
+  validate_bool($carbonserver_metrics_as_counters)
+  validate_string($carbonserver_read_timeout)
+  validate_string($carbonserver_write_timeout)
+  validate_string($carbonserver_scan_frequency)
+
+  validate_bool($dump_enabled)
+  if $dump_path {
+    validate_string($dump_path)
+  }
+  validate_integer($dump_restore_per_second)
 
   # Put the configuration files
   $executable = $go_carbon::executable
--- manifests/params.pp
+++ manifests/params.pp
@@ -43,6 +43,8 @@ class go_carbon::params {
   $internal_graph_prefix              = 'carbon.agents.{host}.'
   # Interval of storing internal metrics. Like CARBON_METRIC_INTERVAL
   $internal_metrics_interval          = '1m0s'
+  # Endpoint for store internal carbon metrics. Valid values: "" or "local", "tcp://host:port", "udp://host:port"
+  $internal_metrics_endpoint          = undef
   # Increase for configuration with multi persisters
   $max_cpu                            = 1
 
@@ -55,12 +57,14 @@ class go_carbon::params {
   $whisper_workers                    = 1
   # Limits the number of whisper update_many() calls per second. 0 - no limit
   $whisper_max_updates_per_second     = 0
+  $whisper_sparse_create              = false
   $whisper_enabled                    = true
 
   # Limit of in-memory stored points (not metrics)
   $cache_max_size                     = 1000000
   # Capacity of queue between receivers and cache
   $cache_input_buffer                 = 51200
+  $cache_write_strategy               = max
 
   $udp_listen                         = '0.0.0.0:2003'
   # Enable optional logging of incomplete messages (chunked by MTU)
@@ -82,6 +86,32 @@ class go_carbon::params {
   $carbonlink_read_timeout            = '30s'
   # Return empty result if cache not reply
   $carbonlink_query_timeout           = '100ms'
+
+  # Carbonserver support is still experimental and may contain bugs
+  # Or be incompatible with github.com/grobian/carbonserver
+  $carbonserver_enabled               = false
+  $carbonserver_listen                = '0.0.0.0:8080'
+  # Buckets to track response times
+  $carbonserver_buckets               = 10
+  # Maximum amount of globs in a single metric
+  $carbonserver_max_globs             = 100
+  # carbonserver-specific metrics will be sent as counters
+  # For compatibility with grobian/carbonserver
+  $carbonserver_metrics_as_counters   = false
+  # Read and Write timeouts for HTTP server
+  $carbonserver_read_timeout          = '60s'
+  $carbonserver_write_timeout         = '60s'
+  # carbonserver keeps track of all available whisper files
+  # in memory. This determines how often it will check FS
+  # for new metrics.
+  $carbonserver_scan_frequency        = '5m0s'
+
+  # Enable dump/restore function on USR2 signal
+  $dump_enabled                       = false
+  # Directory for store dump data. Should be writeable for carbon
+  $dump_path                          = undefA
+  # Restore speed. 0 - unlimited
+  $dump_restore_per_second            = 0
 
   $pprof_listen                       = '127.0.0.1:7007'
   $pprof_enabled                      = false
